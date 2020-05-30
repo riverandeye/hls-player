63425e661525cfd103e8fdcd00be8a724d1637ff
riverandeye <riverandeye@gmail.com>
2020-05-31 00:05:32 +0900

    Updates

---
+++ precache-manifest.a8ba8decb1e297562c277a7631d82e41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4a089c133261c1b47e9ea9d7e87151",
+    "url": "/hls-client/index.html"
+  },
+  {
+    "revision": "50c0276e98f6babc5a9d",
+    "url": "/hls-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bbece9c4159f40eb3ae7",
+    "url": "/hls-client/static/js/2.624bc9aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hls-client/static/js/2.624bc9aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c0276e98f6babc5a9d",
+    "url": "/hls-client/static/js/main.4b1aa380.chunk.js"
+  },
+  {
+    "revision": "e2d111a911a097e0e071",
+    "url": "/hls-client/static/js/runtime-main.c7cbfdc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hls-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
