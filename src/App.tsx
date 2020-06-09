a5d02a04b6f72d98dbc243d06357f7d4aa650753
riverandeye <riverandeye@gmail.com>
2020-06-09 18:55:42 +0900

    add : embed watch page

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 
 import Main from "./view/main";
+import Watch from "./view/watch";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Main} />
+        <Route path="/watch/:streamerId" exact component={Watch} />
         <Redirect from="*" to="/" />
       </Switch>
     </Router>
